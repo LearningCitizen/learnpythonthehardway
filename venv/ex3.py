6446dffad9e344c3b3473d82a74e4a0a93312cd6
hippolyt <john-son.hippolyte-ext@amundi.com>
2019-03-15 18:50:47 +0100

    ex3

---
+++ venv/ex3.py
@@ -0,0 +1,35 @@
+# display a message
+print("I will now count my chickens:")
+
+#display the number of hens
+print ("hens", 25.0 + 30.0 / 6.0)
+#display the number of hens
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
+
+# display a message
+print ("Now I will count the eggs:")
+
+# display the result of an operation
+print (3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+
+# display a message
+print("Is it true that 3 + 2 < 5 - 7")
+
+# display the reasult of 3 + 2 < 5 -7
+print(3.0 + 2.0 < 5.0 - 7.0)
+# display the reasult of 3 + 2
+print("What is 3 + 2?", 3.0 + 2.0)
+# display the reasult of 5 - 7
+print("Whats is 5 - 7?", 5.0 - 7.0)
+
+# display a message
+print("Oh, that's why it's False.")
+# display a message
+print("How about some more.")
+
+# display the reasult of 5 > -2
+print("Is it greater?", 5.0 > -2.0)
+# display the reasult of 5 >= -2
+print("Is is greater or equal?", 5.0 >= -2.0)
+# display the reasult of 5 <= 2
+print("Is it less or equal?", 5.0 <= 2.0)
